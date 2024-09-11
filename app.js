b36f27a94b4c93f1c2d4f95594e66c0e702932dd
Ibtesum <aninda7152@gmail.com>
2024-09-11 17:22:47 +0600

    Create a file using fs module: complete

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const createFile = async (path) => {
+    try {
+      // we want to check whether or not we have that file
+      const existingFileHandle = await fs.open(path, "r");
+      existingFileHandle.close();
+
+      // We already have that file
+      return console.log(`File ${path} already exists`);
+    } catch (error) {
+      // We dont have the file, now we should create it
+      const newFileHandle = await fs.open(path, "w");
+      console.log("A new file was successfully created.");
+      newFileHandle.close();
+    }
+  };
+
+  // commands
+  const CREATE_FILE = "create a file";
+  const DELETE_FILE = "delete a file";
+  const RENAME_FILE = "rename the file";
+  const ADD_TO_FILE = "add to the file";
+
   const commandFileHandler = await fs.open("./command.txt", "r");
 
   commandFileHandler.on("change", async () => {
@@ -16,13 +38,24 @@ const fs = require("fs/promises");
     const position = 0;
 
     // we always want to read the entire file(from start to end)
-    const content = await commandFileHandler.read(
-      buffer,
-      offset,
-      length,
-      position
-    );
-    console.log(content);
+    await commandFileHandler.read(buffer, offset, length, position);
+
+    const command = buffer.toString("utf-8");
+
+    // create a file:
+    // create a file <path>
+    if (command.includes(CREATE_FILE)) {
+      const filePath = command
+        .substring(command.indexOf(CREATE_FILE) + CREATE_FILE.length)
+        .trim();
+      await createFile(filePath);
+    }
+
+    // delete a file 
+    // delete the file <path>
+    if(command.includes(DELETE_FILE)){
+      const filePath = command.substring(DELETE_FILE.length + 1)
+    }
   });
 
   const watcher = fs.watch("./command.txt");
